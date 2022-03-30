b1c86173b2bbac4ca89ee18c3af1b6a4bb7dbdd4
unknown <taha.iu.bd@gmail.com>
2022-03-30 12:04:36 +0600

    gout add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import About from './components/About/About'
+import Friends from './components/Friends/Friends'
+import Home from './components/Home/Home'
+import NotFound from './components/NotFound/NotFound'
 
 function App() {
     return (
         <div className="App">
-            <h1>Hello World!</h1>
+            <h1 className="text-4xl font-bold mb-12">
+                {' '}
+                Hello World! This is Header
+            </h1>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/friends" element={<Friends />} />
+                <Route path="about" element={<About />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </div>
     )
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <img src="./Not Found.gif" alt="" />
+            <h1>jj</h1>
+        </div>
+    )
+}
+
+export default NotFound

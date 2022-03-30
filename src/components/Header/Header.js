ec9db8deaaca083dbf423e86e236eaa60377158c
unknown <taha.iu.bd@gmail.com>
2022-03-30 13:44:56 +0600

    nav add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <div>
+            <nav className="bg-sky-500 mb-8 py-5 text-white text-2xl">
+                <Link className="m-3" to="/">
+                    Home
+                </Link>
+                <Link className="m-3" to="/friends">
+                    Friends
+                </Link>
+                <Link className="m-3" to="/about">
+                    About
+                </Link>
+            </nav>
             <h1 className="text-4xl font-bold mb-5">
                 {' '}
                 Hello World! This is Header
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,6 @@ import React from 'react'
 const NotFound = () => {
     return (
         <div>
-            <img src="./Not Found.gif" alt="" />
             <h1 className="text-red-600 text-4xl mt-12"> Not Found</h1>
             <h1 className="text-red-700 text-9xl mt-12"> 404</h1>
         </div>

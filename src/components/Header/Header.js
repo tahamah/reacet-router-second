907c59e7117b4bfd301f1a160ba349c74647b83a
unknown <taha.iu.bd@gmail.com>
2022-03-30 13:55:12 +0600

    coustom link add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import CustomLink from '../CustomLink/CustomLink'
 
 const Header = () => {
     return (
         <div>
-            <nav className="bg-sky-500 mb-8 py-5 text-white text-2xl">
-                <Link className="m-3" to="/">
+            <nav className="bg-sky-500 mb-8 py-5 flex justify-center text-white  text-2xl">
+                <CustomLink className="m-3" to="/">
                     Home
-                </Link>
-                <Link className="m-3" to="/friends">
+                </CustomLink>
+                <CustomLink className="m-3" to="/friends">
                     Friends
-                </Link>
-                <Link className="m-3" to="/about">
+                </CustomLink>
+                <CustomLink className="m-3" to="/about">
                     About
-                </Link>
+                </CustomLink>
             </nav>
             <h1 className="text-4xl font-bold mb-5">
                 {' '}
